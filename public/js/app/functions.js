5104ab351e83374ba33cad247cc6df28a45e1bd7
root <953372680@qq.com>
2018-06-10 17:27:34 +0800

    精简

--- public/js/app/functions.js
+++ public/js/app/functions.js
@@ -1,3 +1,126 @@
+if (typeof Object.create !== 'function') {
+    Object.create = function (a) {
+        function F() {
+        };F.prototype = a;
+        return new F()
+    }
+}
+
+/*========================================================================================
+ * AvertaScroll2top v1.02. | Copyright (c) averta | http://averta.net
+ *========================================================================================*/
+;(function ($) {
+    var Scroll = {
+        init: function (el, options) {
+            //cache this
+            var self = this;
+            self.options = $.extend({}, $.fn.avertaScroll2top.defaultOptions, options || {});
+            // Access to jQuery and DOM versions of element
+            self.$el = $(el);
+            self.el = el;
+
+            self.setup();
+        },
+        setup: function () {
+            var self = this;
+            if (self.options.autoFade && (self.$el.data("autofade") != false)) self.autofade();
+            self.$el.on("click", function () {
+                $('body,html').animate({scrollTop: 0}, self.options.speed, self.options.ease);
+                return false;
+            });
+        },
+        autofade: function () {
+            var self = this;
+            //hide btn on init
+            if (window.scrollY < self.options.offset) {
+                self.$el.fadeOut(0);
+            }
+            $(window).scroll(function () {
+                if ('pageXOffset' in window) {  // all browsers, except IE before version 9
+                    var topOffset = window.pageYOffset;
+                }
+                else {      // Internet Explorer before version 9
+                    var topOffset = document.documentElement.scrollTop;
+                }
+                if (topOffset > self.options.offset) {
+                    self.$el.fadeIn(self.options.fadeDuration);
+                } else {
+                    self.$el.fadeOut(self.options.fadeDuration);
+                }
+            });
+        }
+    };
+
+    $.fn.avertaScroll2top = function (options) {
+        return this.each(function () {
+            var scroll = Object.create(Scroll);
+            scroll.init(this, options);
+        });
+    };
+
+    $.fn.avertaScroll2top.defaultOptions = {
+        speed: 200,                   // scroll duration in millisecond
+        fadeDuration: 400,            // btn fade duration in millisecond
+        ease: 'linear',              // scroll easing
+        offset: 100,                  // the distance in pixel to autoFade the btn
+        autoFade: true                // specify whether fade the element when scroll offset passed
+    };
+})(jQuery);
+
+/* ================== init.superfish.js =================== */
+;(function ($) {
+    /*--------------------------------------------
+     *   superfish menu init
+     *--------------------------------------------*/
+    function init_superfish(speed, delay, fade) {
+        var animEff = {opacity: 'show', height: 'show'};
+        if (fade) animEff.opacity = 'show';
+        $('ul.sf-menu').superfish({
+            delay: delay,    // one second delay on mouseout
+            animation: animEff,  // fade-in and slide-down animation
+            speed: speed,    // faster animation speed
+            autoArrows: true,     // disable generation of arrow mark-up
+            dropShadows: false     // disable drop shadows
+        });
+    }
+
+    init_superfish('fast', 100, true);
+})(jQuery);
+
+/* ================== init.averta.js =================== */
+;(function ($) {
+    /*--------------------------------------------
+     *  Averta plugins
+     *--------------------------------------------*/
+    $(".scroll2top").avertaScroll2top({ease: 'easeInOutQuint', speed: 800});
+})(jQuery);
+
+/* ================== click.js =================== */
+;(function ($) {
+    $('.nav-toggle').unbind('click')
+        .bind('click', function (event) {
+            event.preventDefault();
+            $(this).toggleClass('active');
+            $('nav#access .sf-menu').animate({height: 'toggle'});
+        });
+})(jQuery);
+
+// position callout button in safari
+;(function ($) {
+    if (!(navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1)) return;
+    var $callout = $('div.callout');
+    var $btn = $callout.find('.featured_btn');
+    var $label = $btn.find('span');
+
+    function updateCalloutBtnPosition() {
+        var topPos = ($btn.height() - $label.height()) * 0.5;
+        $label.css('top', topPos);
+    }
+
+    updateCalloutBtnPosition();
+    $(window).bind("resize", updateCalloutBtnPosition);
+})(jQuery);
+
 //提示消息框
 function tip(options) {
     var conf = {
--- public/js/main.js
+++ public/js/main.js
@@ -1,39 +1,26 @@
 requirejs.config({
-    //By default load any module IDs from js/lib
     baseUrl: '/js/',
-    /*urlArgs: function (id, url) {
-        var args = 'v=201805241516';
-        if (url.indexOf('libs') !== -1) {
-            args = 'v=1';
-        }
-
-        return (url.indexOf('?') === -1 ? '?' : '&') + args;
-    },*/
-
-    //except, if the module ID starts with "app",
-    //load it from the js/app directory. paths
-    //config is relative to the baseUrl, and
-    //never includes a ".js" extension since
-    //the paths config could be for a directory.
     paths: {
-        "jquery": '//apps.bdimg.com/libs/jquery/2.1.4/jquery.min',
-        "angular": '//apps.bdimg.com/libs/angular.js/1.4.6/angular.min',
-        "angular-ui-router": "//apps.bdimg.com/libs/angular-ui-router/0.2.15/angular-ui-router.min",
-        "laravel": "app/app",
-        "angular-async-loader": "libs/angular-async-loader.min",
-        "ctrl": 'app/controllers',
-        "srv": 'app/services',
-        "dire": 'app/directives',
-        "config": 'app/config',
-        "functions": 'app/functions'
+        'jquery': '//apps.bdimg.com/libs/jquery/2.1.4/jquery.min',
+        'angular': '//apps.bdimg.com/libs/angular.js/1.4.6/angular.min',
+        'angular-ui-router': '//apps.bdimg.com/libs/angular-ui-router/0.2.15/angular-ui-router.min',
+        'superfish': '//cdn.bootcss.com/superfish/1.7.9/js/superfish.min',
+        'easing': '//cdn.bootcss.com/jquery-easing/1.3/jquery.easing.min',
+        'laravel': 'app/app',
+        'angular-async-loader': 'libs/angular-async-loader.min',
+        'ctrl': 'app/controllers',
+        'srv': 'app/services',
+        'dire': 'app/directives',
+        'config': 'app/config',
+        'functions': 'app/functions',
     },
     shim: {
-        "functions": ["jquery"],
+        'functions': ['jquery', 'superfish', 'easing'],
         'angular': {
             exports: 'angular'
         },
         'angular-ui-router': {
-            deps: ["angular"]
+            deps: ['angular']
         },
         'config': {
             deps: ['angular'],
@@ -43,9 +30,9 @@ requirejs.config({
 });
 
 // Start the main app logic.
-requirejs(['jquery', 'angular', 'laravel', 'ctrl/loadingInterceptor', 'angular-ui-router', 'app/routers', 'app/filter'],
+requirejs(['jquery', 'angular', 'laravel', 'ctrl/loadingInterceptor', 'angular-ui-router', 'app/routers'],
     function ($, angular) {
         $(function () {
-            angular.bootstrap(document, ['laravel'])
+            angular.bootstrap(document, ['laravel']);
         });
     });
